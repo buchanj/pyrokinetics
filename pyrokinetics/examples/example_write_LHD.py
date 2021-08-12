9f7e543dc376d58cecdd01b0038bfdaf35010084
Buchanan, James <james.buchanan@ukaea.uk>
2021-08-12 15:30:39 +0100

    Forgot to add file in previous commit.

--- pyrokinetics/examples/example_write_LHD.py
+++ pyrokinetics/examples/example_write_LHD.py
@@ -5,31 +5,19 @@ import numpy as np
 # Point to input files
 templates = os.path.join('..', 'templates')
 
-# Equilibrium file
-eq_file = os.path.join(templates, 'test.geqdsk')
-
-# Kinetics data file
-kinetics_file = os.path.join(templates, 'jetto.cdf')
+# Template input file
+gs2_file = os.path.join(templates, 'james_step.in')
 
 # Load up pyro object
-pyro = Pyro(eq_file=eq_file, eq_type='GEQDSK', kinetics_file=kinetics_file, kinetics_type='JETTO')
-
-# Generate local parameters at psi_n=0.5
-pyro.load_local(psi_n=0.5, local_geometry='Miller')
-
-# Change GK code to GS2
-pyro.gk_code = 'GS2'
-
-# Write single input file using my own template
-pyro.write_gk_file(file_name='test_jetto.gs2', template_file='step.in')
+pyro = Pyro(gk_file=gs2_file, gk_type='GS2')
 
 # Use existing parameter
 param_1 = 'ky'
 values_1 = np.arange(0.1, 0.3, 0.1)
 
-# Add new parameter to scan through
+# Add new parameter
 param_2 = 'my_electron_gradient'
-values_2 = np.arange(0.0, 1.5, 0.5)
+values_2 = np.arange(2.5, 4.0, 0.5)
 
 # Dictionary of param and values
 param_dict = {param_1: values_1,
@@ -40,15 +28,14 @@ pyro_scan = PyroLHD(pyro,
                     param_dict,
                     value_fmt='.3f',
                     value_separator='_',
-                    parameter_separator='_',
-                    file_name='mygs2.in'
+                    parameter_separator='_'
 )
 
 pyro_scan.add_parameter_key(param_2, 'local_species', ['electron', 'a_lt'])
 
-pyro_scan.write(npoints=10, directory='test_GS2_LHD')
+pyro_scan.write(npoints=10, directory='test_GS2_LHD', template_file=gs2_file)
 
 image_name     = 'gs2_local'
 max_containers = 10 
 
-#pyro_scan.run_docker_local(image_name,max_containers)
+pyro_scan.run_docker_local(image_name,max_containers)
